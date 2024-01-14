7741391f43db1c98c0b9773d7d1af99d9fc338d5
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2024-01-15 00:42:45 +0600

    project completed

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,7 @@ function Sqaure({ value, onSquareClick }) {
     </button>
   );
 }
-export default function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
-
+function Board({xIsNext, squares, onPlay}) {
   const winner = calculateWinner(squares);
   let status;
 
@@ -27,8 +24,7 @@ export default function Board() {
     if (squares[i] || calculateWinner(squares)) return;
     const nextSquare = squares.slice();
     xIsNext ? (nextSquare[i] = "X") : (nextSquare[i] = "O");
-    setSquares(nextSquare);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquare);
   }
 
   return (
@@ -50,6 +46,51 @@ export default function Board() {
         <Sqaure value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
+  );
+}
+
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [xIsNext, setXIsNext] = useState(true);
+  const [currentMove, setCurrentMove] = useState(0);
+
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    setXIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(move) {
+    setCurrentMove(move);
+    setXIsNext(move % 2 === 0);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if(move > 0) {
+      description = `Go to the move # ${move}`;
+    } else {
+      description = `Go to start the game`;
+    }
+    return (
+      <li key={move} className="bg-gray-700 text-white mb-1 p-1 rounded-sm">
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="flex justify-center p-4">
+      <div className="mr-16">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div>
+        <ol className="border border-gray-400 p-1 text-lg">{moves}</ol>
+      </div>
+    </div>
   );
 }
 
